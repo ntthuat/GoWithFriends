93376aeb79f306ec36e18247887630cc6f16e66e
ntthuat <nguyenthienthuat92@gmail.com>
2018-12-27 23:56:54 +0700

    abc

---
+++ type script tutorial/arrayExample.js
@@ -0,0 +1,7 @@
+var alphas;
+alphas = ["1", "2", "3", "4"];
+console.log(alphas[0]);
+console.log(alphas[1]);
+// Push() example
+alphas.push("5");
+console.log(alphas[4]);
---
+++ type script tutorial/arrayExample.ts
@@ -0,0 +1,8 @@
+let alphas:string[];
+alphas = ["1","2","3","4"]
+console.log(alphas[0]);
+console.log(alphas[1]);
+
+// Push() example
+alphas.push("5");
+console.log(alphas[4]);
