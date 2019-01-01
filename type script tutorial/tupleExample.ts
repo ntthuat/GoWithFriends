a7bada79627933438abf81ea25a7ad3e5a69d52b
ntthuat <nguyenthienthuat92@gmail.com>
2019-01-01 14:20:46 +0700

    + can't complete import export

---
+++ type script tutorial/tupleExample.ts
@@ -0,0 +1,7 @@
+var mytuple = [10, "Hello"]; //create a  tuple
+logTypeAndValue(mytuple[0]);
+logTypeAndValue(mytuple[1]);
+
+function logTypeAndValue(input) {
+    console.log(typeof input + ': ' + input);
+}
---
+++ type script tutorial/unionExample.ts
@@ -0,0 +1,7 @@
+var val:string|number;
+val = 12;
+console.log("numeric value of val "+val);
+console.log(typeof val);
+val = "This is a string";
+console.log("string value of val "+val);
+console.log(typeof val);
